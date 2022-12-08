54bd83432a02deaa054ed5a2dfca9a6e0757d287
Saiful Rahman <52599620+Saiful234@users.noreply.github.com>
2022-12-08 10:52:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-from datetime import datetime
-import pandas
+import datetime
 import random
 import smtplib
 
-MY_EMAIL = "YOUR EMAIL"
-MY_PASSWORD = "YOUR PASSWORD"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+my_email = "bandotech46@gmail.com"
+my_password = "ildoygqbxpeggivi"
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+now = datetime.datetime.now()
+weekday = now.weekday()
+if weekday == 6:
+with open("quotes.txt") as quote_file:
+    all_quote = quote_file.readlines()
+    quote = random.choice(all_quote)
+    print(quote)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+with smtplib.SMTP("smtp.gmail.com", 587, timeout=120) as connection:
+    connection.starttls()
+    connection.login(my_email, my_password)
+    connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject: Monday Motivationn\n\n{quote}")
+
